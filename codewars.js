c9127d63edc70cfcecaed204f105ef0bb8de1fe8
Jalen Battle <jalenbattle78@gmail.com>
2024-11-13 21:11:26 -0500

    wow practice

--- codewars.js
+++ codewars.js
@@ -3,10 +3,14 @@ function findDifference(a, b) {
     const sumA = a[0] * a[1] * a[2]; // Multiply each number in a (e.g. [1, 2, 3])
     const sumB = b[0] * b[1] * b[2]; // Multiply each number in b (e.g. [4, 5, 6])
     return Math.abs(sumA - sumB) // Return the difference as the absolute value (no negative)
-  }
-  console.log(findDifference([3, 2, 5], [1, 4, 4]))
+}
 
+function find_difference(a, b) {
+    return Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2]); // Just do it all on one line duh
+}
 
-  function find_difference(a, b) {
-    return Math.abs(a[0]*a[1]*a[2]-b[0]*b[1]*b[2]); // Just do it all on one line duh
-  }
+function find_difference(a, b) {
+    return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+} // .reduce is used here to multiply each number in the list/array (e.g. [a * b = total * c] - [a * b = total * c])
+
+//////////////////////////////////////////////////////////////////////////////////////////////
