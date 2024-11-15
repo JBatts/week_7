c0b6f2c31cbbaf7f8495d192f703baec8c48e8be
Jalen Battle <jalenbattle78@gmail.com>
2024-11-15 11:18:09 -0500

    class stuff

---
+++ bookmarks/data.js
@@ -0,0 +1,13 @@
+
+const folders = [ // Categories?
+    "Html", // index == 0
+    "CSS", // index == 1
+    "JavaScript", // index == 2
+    "Other Design Resources", // index == 3
+    "..."
+]; // End Folders
+
+const links = [
+    {folder: 0, display_name: "MDM", desc: "Mozilla...", href: "http://developer.mozilla.org"},
+    {folder: 0, display_name: "W3S", desc: "W3 School", href: "http://www.w3schools.com/"},
+] // End links
